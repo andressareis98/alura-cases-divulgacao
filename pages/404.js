fbac016dc32a90b1452a36034336cbb04239f120
Andressa Reis Araujo <andressareis98@outlook.com>
2022-07-15 15:35:26 -0300

    feat: create link component

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Pag404(){
+    return(
+        <div>
+            <h1>Você caiu na página 404</h1>
+        </div>
+    )
+}
--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
+import Link from '../src/components/Link'
+
 export default function FaqPage(){
     return(
         <div>
             <h1>Alura Cases - Página de Perguntas FAQ</h1>
-            <a href="/">Ir para a home</a>
+            <Link href="/">
+                Ir para home
+            </Link>
         </div>
     )
 }
